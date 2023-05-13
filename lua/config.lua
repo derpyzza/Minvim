a94719f6262198a7a74c2283f88bdef823f2fe1d
derpyzza <derpyzza@gmail.com>
2023-05-13 12:26:45 +0500

    mouse fix

--- lua/config.lua
+++ lua/config.lua
@@ -1,3 +1,26 @@
+local function getWords()
+  if vim.bo.filetype == "md" or vim.bo.filetype == "txt" or vim.bo.filetype == "markdown" then
+    if vim.fn.wordcount().visual_words == 1 then
+      return tostring(vim.fn.wordcount().visual_words) .. " word"
+    elseif not (vim.fn.wordcount().visual_words == nil) then
+      return tostring(vim.fn.wordcount().visual_words) .. " words"
+    else
+      return tostring(vim.fn.wordcount().words) .. " words"
+    end
+  else
+    return ""
+  end
+end
+
+local function StatusLine()
+	stl = ""
+	stl = stl .. "λ "
+	stl = stl .. "%#STATUS#%f%m%r%h%w: %= %##%2l»%L %#FILETYPE#%y "
+	stl = stl .. getWords()
+
+	return stl
+end
+
 options = {
 	ru            = true,
 	list          = true,
@@ -12,6 +35,7 @@ options = {
 	cursorline		= true,
 
 	mouse         = "a",
+	mousemodel 		= "extend",
 	tabstop       = 2,
 	shiftwidth    = 0,
 	scrolloff     = 4,
@@ -20,7 +44,7 @@ options = {
 	fillchars			= "eob: ",
 	incsearch 		= true,
 	clipboard     = "unnamedplus",
-	statusline    = "λ %#STATUS#%f%m%r%h%w: %= %##%2l»%L %#FILETYPE#%y",
+	statusline    = StatusLine(),
 	cursorlineopt = "number",
 	completeopt = 'menu,menuone'
 }
